fe4341c4b48b5eeabef5ba581b0504e648ee2b12
Christian Pellegrin <chri@evolware.org>
2015-12-12 11:09:02 +0000

    B ndroid project created

---
+++ blue_light_meter_android/src/org/evolware/bluelightmeter/BlueLightMeterActivity.java
@@ -0,0 +1,15 @@
+package org.evolware.bluelightmeter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlueLightMeterActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
